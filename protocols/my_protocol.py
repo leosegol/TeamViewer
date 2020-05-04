3d8e314f22cd244e1d39e2b0d937680d8fc652ee
Leo <leosegol@gmail.com>
2020-05-04 14:56:20 +0300

    dual socket server

--- protocols/my_protocol.py
+++ protocols/my_protocol.py
@@ -4,14 +4,16 @@ MSG_LENGTH = 7
 
 
 def send(client_socket, msg):
-    length = str(len(msg)).zfill(MSG_LENGTH).encode()
-    msg = length + msg
-    print(msg)
+
     while msg:
         if len(msg) > 10000:
-            client_socket.send(msg[0:10000])
+            length = str(len(msg)).zfill(MSG_LENGTH).encode()
+            send_ = length + msg[0:10000]
+            client_socket.send(send_)
         else:
-            client_socket.send(msg)
+            length = str(len(msg)).zfill(MSG_LENGTH).encode()
+            send_ = length + msg
+            client_socket.send(send_)
             break
         msg = msg[10000:]
 
